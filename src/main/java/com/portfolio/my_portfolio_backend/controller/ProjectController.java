a131c3534c5314b7b496cd67bdbe8cb7c4421d4c
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-02-10 10:08:52 +0100

    feat: Implement server-side validation for projects
    - Add BindingResult to ProjectController to catch form errors
    - Update form-project.html to display Thymeleaf error messages
    - Style validation errors with custom CSS class

--- src/main/java/com/portfolio/my_portfolio_backend/controller/ProjectController.java
+++ src/main/java/com/portfolio/my_portfolio_backend/controller/ProjectController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -40,9 +41,16 @@ public class ProjectController {
 
 
     @PostMapping("/save")
-    public String saveProject(@Valid @ModelAttribute("projectDto") ProjectDto projectDto,
+    public String saveProject(@Valid @ModelAttribute("projectDto") ProjectDto projectDto, BindingResult result,
                               @RequestParam("file") MultipartFile file
     ) {
+        if(file.isEmpty()){
+            result.rejectValue("imageUrl","file.required","La imagen es obligatoria");
+        }
+
+        if(result.hasErrors()){
+            return "projects/form-project";
+        }
         try {
             String imageUrl = fileStorageService.storeFile(file);
 
