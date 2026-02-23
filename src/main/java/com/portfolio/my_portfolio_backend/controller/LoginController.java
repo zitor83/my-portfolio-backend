5c711533f4df7f9de20385d4bfaa90f7875d12ca
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-02-23 10:15:49 +0100

    feat: Enhance login UX with error handling and redirect
    - Add error and success message blocks to form-login.html
    - Update LoginController to handle Spring Security ?error parameter
    - Implement authentication check to redirect already logged-in users away from the login page

--- src/main/java/com/portfolio/my_portfolio_backend/controller/LoginController.java
+++ src/main/java/com/portfolio/my_portfolio_backend/controller/LoginController.java
@@ -1,13 +1,24 @@
 package com.portfolio.my_portfolio_backend.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String showLoginForm(){
+    public String showLoginForm(@RequestParam(value = "error", required = false) String error, Model model,
+                                Authentication authentication
+                                ){
+        if(authentication != null && authentication.isAuthenticated()){
+            return "redirect:/";
+        }
+        if(error != null){
+            model.addAttribute("error", "Usuario o contraseña incorrectos");
+        }
         return "auth/form-login";
     }
 
